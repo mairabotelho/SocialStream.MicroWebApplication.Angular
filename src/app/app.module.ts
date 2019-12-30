019e1e521c32cd03453c436f9a0cffdf0e15a8e3
Amendoza20 <20.angel.mendoza@gmail.com>
2019-12-30 12:43:55 -0500

    added user and video component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoComponent } from './component/video/video.component';
+import { UserComponent } from './component/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
