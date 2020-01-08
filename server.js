2a670f6f1fb946ebd45205a691783b90beb9deaa
Amendoza20 <20.angel.mendoza@gmail.com>
2020-01-08 09:50:06 -0500

    push serer.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/SocialStream-MicroWebApplication-ui'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/SocialStream-MicroWebApplication-ui/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/SocialStream-MicroWebApplication-ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
